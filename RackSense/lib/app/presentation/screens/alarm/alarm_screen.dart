945a8d26470a05507bea8cfc63ca48b5a98b3846
ilker <ilker@okutman.com>
2026-07-20 15:26:28 +0300

    Build: 2026-07-20 15:26:28

--- RackSense/lib/app/presentation/screens/alarm/alarm_screen.dart
+++ RackSense/lib/app/presentation/screens/alarm/alarm_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:rack_sense/app/presentation/components/app_scaffold.dart';
+import 'package:rack_sense/app/presentation/screens/alarm/widgets/alarm_list_card.dart';
 
 class AlarmScreen extends StatelessWidget {
   const AlarmScreen({super.key});
@@ -8,8 +9,18 @@ class AlarmScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppScaffold(
       selectedIndex: 2,
-      title: 'Alarm',
-      body: Center(child: Text('TODO:  Alarm sayfasi')),
+      title: 'Alarm Yönetimi',
+      body: Row(
+        spacing: 12,
+        children: [
+          Expanded(
+            child: AlarmListCardWidget(title: 'AKTİF ALARMLAR', items: []),
+          ),
+          Expanded(
+            child: AlarmListCardWidget(title: 'ALARM GEÇMİŞİ', items: []),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ RackSense/lib/app/presentation/screens/alarm/widgets/alarm_list_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AlarmListCardWidget extends StatelessWidget {
+  const AlarmListCardWidget({
+    super.key,
+    required this.title,
+    required this.items,
+    this.icon,
+  });
+  final String title;
+  final List<String> items;
+  final Widget? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 20, vertical: 12),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(title),
+              subtitle: Text('${items.length} kayıt'),
+              trailing: icon,
+            ),
+            Expanded(
+              child: items.isNotEmpty
+                  ? ListView.builder(
+                      itemBuilder: (context, index) => Text(items[index]),
+                      itemCount: items.length,
+                    )
+                  : Center(child: Text('Kayıt bulunmuyor')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
