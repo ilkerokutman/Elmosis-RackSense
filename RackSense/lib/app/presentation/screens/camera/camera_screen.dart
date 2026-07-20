71b73459b103b61eccba475de0e82adf6e930289
ilker <ilker@okutman.com>
2026-07-20 15:33:02 +0300

    Build: 2026-07-20 15:33:02

--- RackSense/lib/app/presentation/screens/camera/camera_screen.dart
+++ RackSense/lib/app/presentation/screens/camera/camera_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rack_sense/app/presentation/components/app_scaffold.dart';
 
@@ -8,8 +9,48 @@ class CameraScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppScaffold(
       selectedIndex: 3,
-      title: 'Kamera',
-      body: Center(child: Text('TODO: Ayarlar sayfasi')),
+      title: 'Kamera ve Güvenlik',
+      body: Row(
+        spacing: 12,
+        children: [
+          Expanded(
+            child: Card(
+              child: Padding(
+                padding: EdgeInsetsGeometry.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text('CANLI KAMERA'),
+                      trailing: Text(
+                        'YAPILANDIRILMADI',
+                        style: Theme.of(
+                          context,
+                        ).textTheme.labelSmall?.copyWith(color: Colors.orange),
+                      ),
+                    ),
+                    Expanded(child: Container(color: Colors.deepPurpleAccent)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 180,
+            child: Card(
+              child: Padding(
+                padding: EdgeInsetsGeometry.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                child: Column(children: [Text('Güvenlik')]),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
