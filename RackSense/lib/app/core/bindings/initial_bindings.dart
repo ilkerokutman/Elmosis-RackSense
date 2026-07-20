48a6fe3fa2cb70d333d64f5eca7302cdaef9dcc5
ilker <ilker@okutman.com>
2026-07-20 11:51:40 +0300

    Build: 2026-07-20 11:51:40

--- RackSense/lib/app/core/bindings/initial_bindings.dart
+++ RackSense/lib/app/core/bindings/initial_bindings.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:rack_sense/app/data/controllers/business_controller.dart';
+import 'package:rack_sense/app/data/controllers/app_controller.dart';
 import 'package:rack_sense/app/data/controllers/gpio_controller.dart';
 import 'package:rack_sense/app/data/controllers/main_controller.dart';
 import 'package:rack_sense/app/data/controllers/sync_controller.dart';
@@ -15,12 +15,15 @@ class InitialBindings extends Bindings {
     await connectivityService.initialize();
     await Get.putAsync(() async => connectivityService, permanent: true);
 
+    print("Dependency Injection: Starting GPIO init");
     await Get.putAsync(() async => GpioController(), permanent: true);
     await Get.find<GpioController>().initialize();
+    print("Dependency Injection: GPIO init completed");
 
     await Get.putAsync(() async => SyncController(), permanent: true);
-    await Get.putAsync(() async => BusinessController(), permanent: true);
 
     await Get.putAsync(() async => MainController(), permanent: true);
+
+    await Get.putAsync(() async => AppController(), permanent: true);
   }
 }
