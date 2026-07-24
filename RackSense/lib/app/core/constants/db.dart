eda8b1262f8cf9aa3fdb87bba9ff725c7e505297
ilker <ilker@okutman.com>
2026-07-24 12:48:27 +0300

    Build: 2026-07-24 12:48:27

--- RackSense/lib/app/core/constants/db.dart
+++ RackSense/lib/app/core/constants/db.dart
@@ -1,5 +1,5 @@
 class DbConstants {
   static const int dbVersion = 2;
-  static const String dbPath = '/opt/rack_sense_data';
+  static const String dbPath = '/home/pi/.local/share/rack_sense';
   static const String dbFile = 'rack_sense.db';
 }
