febe938ebb61438a2c1c5c5b5a3239953add90a0
ilker <ilker@okutman.com>
2026-07-27 09:52:48 +0300

    Build: 2026-07-27 09:52:48

--- RackSense/lib/app/presentation/screens/dashboard/temperature_card.dart
+++ RackSense/lib/app/presentation/screens/dashboard/temperature_card.dart
@@ -10,18 +10,27 @@ class TemperatureControlCardWidget extends StatelessWidget {
   final int value;
   final VoidCallback onDecrease;
   final VoidCallback onIncrease;
+  // actual temperature
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
-            Expanded(child: Text('$value°C')),
+            Expanded(
+              child: Center(
+                child: Text(
+                  '$value°C', // TODO: this shall be the actual average environment temperature
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -32,7 +41,7 @@ class TemperatureControlCardWidget extends StatelessWidget {
                 SizedBox(
                   width: 96,
                   child: Text(
-                    '$value°C',
+                    '$value°C', // this is the target set value
                     textAlign: TextAlign.center,
                     style: Theme.of(context).textTheme.headlineMedium,
                   ),
